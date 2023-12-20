1ff99d2344659d04ab2007140afe958f4472a53d
Aayush kumar <aayushkumarhigh@gmail.com>
2023-12-20 11:19:07 +0530

    text fixes

--- app/(userAction)/dashboard/page.jsx
+++ app/(userAction)/dashboard/page.jsx
@@ -59,20 +59,31 @@ const Page = () => {
     <Loader />
   ) : (
     <div className="flex min-h-[83vh] items-center flex-col gap-10">
-        <div className="w-full px-10  flex gap-8 ">
-          <div>
-            <div className="text-3xl tracking-wide  font-medium  font-sans">Hello There</div>
-            <div className=" text-2xl px-1 text-slate-700 font-serif">Welcome</div></div>
-          <div>
-            {isAdmin && <Button
-              className="w-[90%] m-auto shadow-lg bg-[#cd393e] text-stone-200 text-lg font-bold tracking-wide"
-              variant="primary"
-              size="lg">
-              <Link href={"/complain"}>Admin Dashboard</Link>
-            </Button>}
+      <div className="w-full px-10  flex gap-8 ">
+        <div>
+          <div className="text-3xl tracking-wide  font-medium  font-sans">
+            Hello Resident
+          </div>
+          <div className="text-2xl text-slate-700 tracking-wide  font-medium  font-sans">
+            Ashiana Suncity
+          </div>
+          <div className=" text-2xl px-1 text-slate-700 font-serif">
+            Welcome in our Complain box
           </div>
         </div>
-        <div className="bg-slate-200 w-[90%] rounded-lg py-16 flex flex-col justify-center items-center px-10">
+        <div>
+          {isAdmin && (
+            <Button
+              className="w-[90%] m-auto shadow-lg bg-[#cd393e] text-stone-200 text-lg font-bold tracking-wide"
+              variant="primary"
+              size="lg"
+            >
+              <Link href={"/complain"}>Admin Dashboard</Link>
+            </Button>
+          )}
+        </div>
+      </div>
+      <div className="bg-slate-200 w-[90%] rounded-lg py-16 flex flex-col justify-center items-center px-10">
         <Button
           className="w-[90%] m-auto shadow-lg bg-[#cd393e] text-stone-200 text-lg font-bold tracking-wide"
           variant="primary"
@@ -104,7 +115,6 @@ const Page = () => {
           Logout
         </Button>
       </div>
-
     </div>
   );
 };
